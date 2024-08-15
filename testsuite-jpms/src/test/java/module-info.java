81a064d1ad04ff07b73c559a014f5316ba3c1777
Julien Viet <julien@julienviet.com>
2024-08-16 00:04:58 +0200

    codec-xml

--- testsuite-jpms/src/test/java/module-info.java
+++ testsuite-jpms/src/test/java/module-info.java
@@ -2,6 +2,7 @@ open module io.netty.testsuite_jpms.test {
     requires org.junit.jupiter.api;
     requires io.netty.buffer;
     requires io.netty.codec;
+    requires io.netty.codec.xml;
     requires io.netty.handler;
     requires io.netty.transport.classes.kqueue;
     requires io.netty.resolver.dns.classes.macos;
